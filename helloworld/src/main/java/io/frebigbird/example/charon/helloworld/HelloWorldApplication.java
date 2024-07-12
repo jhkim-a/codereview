532ae378dec19a71d54eebb2f08cc8450d18349e
지훈 <44892297+jhkim-a@users.noreply.github.com>
2024-07-12 20:59:10 +0900

    Update HelloWorldApplication.java

--- helloworld/src/main/java/io/frebigbird/example/charon/helloworld/HelloWorldApplication.java
+++ helloworld/src/main/java/io/frebigbird/example/charon/helloworld/HelloWorldApplication.java
@@ -1,7 +1,7 @@
 package io.frebigbird.example.charon.helloworld;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.*;
 
 @SpringBootApplication
 public class HelloWorldApplication {
